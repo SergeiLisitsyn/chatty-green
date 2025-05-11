a65b021da3dd7fac19d4b66acfa3d97fefb479be
Olha Faruk <farukolga2017@gmail.com>
2025-05-11 12:13:58 +0200

    Модель Subscription: subscriber → author

--- subscriptions/models.py
+++ subscriptions/models.py
@@ -1,3 +1,31 @@
+# subscriptions/models.py
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+class Subscription(models.Model):
+    subscriber = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='subscribed_authors'# через это имя можно получить список авторов, на которых подписан пользователь
+    )
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='followers'# через это имя можно получить список подписчиков пользователя (автора)
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    # Временное поле для обнаружения изменений
+    #temp_field = models.IntegerField(default=0)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=['subscriber', 'author'], name='unique_subscription'),
+        ]
+        verbose_name = "Подписка"
+        verbose_name_plural = "Подписки"
+
+    def __str__(self):
+        return f"{self.subscriber} подписан на {self.author}"
+
