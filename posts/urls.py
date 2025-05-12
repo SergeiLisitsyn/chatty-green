b472bfa5660bb31034aa8fa2804fec52a23d8aff
YuliiaRyzhanina <yryzhanina@gmail.com>
2025-05-12 22:52:44 +0300

    лента постов подписок

--- posts/urls.py
+++ posts/urls.py
@@ -1,20 +1,22 @@
 from django.urls import path
 from . import views
-from .views import PostUpdateView, PostDeleteView, PostCreateView, PostDetailView
+from .views import PostUpdateView, PostDeleteView, PostCreateView, PostDetailView, like_post, dislike_post
 from django.views.generic import TemplateView
 from django.contrib.auth.views import LoginView
+from .views import FeedView
+
 
 urlpatterns = [
-    path('home/', TemplateView.as_view(template_name='home.html'), name='home'),
-    path('login/', LoginView.as_view(template_name='login.html'), name='login'),
-    path('', views.PostListView.as_view(), name='post_list'),                        # Список постов
-    path('create/', PostCreateView.as_view(), name='post_create'),                   # Создание поста
-    path('<slug:slug>/edit/', PostUpdateView.as_view(), name='post_edit'),           # Редактирование
-    path('<slug:slug>/delete/', PostDeleteView.as_view(), name='post_delete'),       # Удаление
-    path('<slug:slug>/like/', views.like_post, name='like_post'),                    # AJAX-лайк
-    path('<slug:slug>/dislike/', views.dislike_post, name='dislike_post'),           # AJAX-дизлайк
-    path('<slug:slug>/', views.PostDetailViewSlug.as_view(),name='post_detail_slug'),
-    path('id/<int:pk>/', views.PostDetailViewId.as_view(), name='post_detail_id'),
-
+    path('', views.PostListView.as_view(), name='post_list'),                       # Список постов
+    path('create/', views.PostCreateView.as_view(), name='post_create'),                # Страница создания поста
+    path('posts/<int:pk>/', views.PostDetailView.as_view(), name='post_detail_by_id'),
+    # path('posts/<int:post_id>/', views.post_detail, name='post_detail'),
+    path('<slug:slug>/', views.PostDetailView.as_view(), name='post_detail'),     # Страница детали поста
+    path('<slug:slug>/edit/', views.PostUpdateView.as_view(), name='post_edit'),       # Страница редактирования поста
+    path('<slug:slug>/update/', PostUpdateView.as_view(), name='post_update'),  # редактировать пост доступно только автору поста
+    path('<slug:slug>/delete/', views.PostDeleteView.as_view(), name='post_delete'),    # Страница удаления поста
+    path('<slug:slug>/', PostDetailView.as_view(), name='post_detail'),#Этот маршрут позволяет просматривать посты по их `slug`.
+    path('<slug:slug>/like/', like_post, name='like_post'),  #Этот маршрут обрабатывает лайк для поста.
+    path('<slug:slug>/dislike/', dislike_post, name='dislike_post'),  # ✅ Должен быть этот маршрут!
+    path('feed/', FeedView.as_view(), name='feed'),
 ]
-
