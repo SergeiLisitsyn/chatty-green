468c33739c7ec0f37dd6ee470d4ca95641ba1c0c
Sergei Lisitsyn <autoanastasia@narod.ru>
2025-05-13 18:46:55 +0200

    posts, users, subscriptions admin files created

--- posts/admin.py
+++ posts/admin.py
@@ -3,6 +3,7 @@ from .models import Post
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
-    list_display = ('author', 'publication_date', 'slug')
-    search_fields = ('text', 'author__username')
+    list_display = ('title','author', 'publication_date', 'slug')
+    search_fields = ('text', 'title', 'author__username')
     list_filter = ('publication_date',)
+    actions = ['mark_deleted']
