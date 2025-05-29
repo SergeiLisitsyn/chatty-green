4035235c74c66c9556de7e3e02954beef98439ec
Sergei Lisitsyn <autoanastasia@narod.ru>
2025-05-29 12:35:36 +0200

    entry_poimnt is changed

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,7 +6,7 @@ echo "==========================="
 
 # Ожидание базы данных
 ./wait-for-db.sh
-
+gunicorn chatty.wsgi:application --bind 0.0.0.0:${PORT:-10000}
 # Миграции (только для web-сервиса)
 if [ "$RUN_MIGRATIONS" = "true" ]; then
     python manage.py migrate --no-input
