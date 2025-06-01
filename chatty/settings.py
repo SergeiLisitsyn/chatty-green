e7968bed34b1f3d59410a4d1aba4c3bb22d6977f
Sergei Lisitsyn <autoanastasia@narod.ru>
2025-06-01 23:51:54 +0200

    ald data base connection is returned

--- chatty/settings.py
+++ chatty/settings.py
@@ -130,9 +130,9 @@ WSGI_APPLICATION = 'chatty.wsgi.application'
     }
 }
 DATABASE_URL = os.getenv("DATABASE_URL")'''
-DATABASES = {
-    'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
-}
+#DATABASES = {
+#    'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
+#}
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
 
