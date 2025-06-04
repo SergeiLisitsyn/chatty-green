21bd2968792b58cf6d5e10512b23ddf56772271f
Sergei Lisitsyn <autoanastasia@narod.ru>
2025-06-04 16:36:52 +0200

    setting is refacÐtored добавлена аутентификация чÐя

--- chatty/settings.py
+++ chatty/settings.py
@@ -78,6 +78,7 @@ INSTALLED_APPS = [
     'allauth.account',
     'allauth.socialaccount',
     'allauth.socialaccount.providers.github',
+    'allauth.socialaccount.providers.google',
 
 
     # Мои приложения
