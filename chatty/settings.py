13f8823891c79965ea7d57adde411411a2616724
Sergei Lisitsyn <77849154+SergeiLisitsyn@users.noreply.github.com>
2025-06-07 14:25:59 +0200

    Update settings.py

--- chatty/settings.py
+++ chatty/settings.py
@@ -20,7 +20,16 @@ from django.urls import reverse_lazy
 import dj_database_url
 import boto3
 import logging
+from botocore.client import Config
 
+s3 = boto3.client(
+    's3',
+    aws_access_key_id=settings.AWS_ACCESS_KEY_ID,
+    aws_secret_access_key=settings.AWS_SECRET_ACCESS_KEY,
+    region_name=settings.AWS_S3_REGION_NAME,
+    config=Config(signature_version='s3v4')
+)
+print("Bucket location:", s3.get_bucket_location(Bucket=settings.AWS_STORAGE_BUCKET_NAME))
 # Включение детального лога boto3
 boto3.set_stream_logger('botocore', logging.DEBUG)
 
