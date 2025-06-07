d9c1a42c111ac32141b7e7923230ddc45d23990f
Sergei Lisitsyn <77849154+SergeiLisitsyn@users.noreply.github.com>
2025-06-07 14:49:39 +0200

    Update settings.py

--- chatty/settings.py
+++ chatty/settings.py
@@ -364,12 +364,12 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'  # для collectstatic
 STATICFILES_DIRS = [BASE_DIR / 'static']  
 s3 = boto3.client(
     's3',
-    aws_access_key_id=settings.AWS_ACCESS_KEY_ID,
-    aws_secret_access_key=settings.AWS_SECRET_ACCESS_KEY,
-    region_name=settings.AWS_S3_REGION_NAME,
+    aws_access_key_id=AWS_ACCESS_KEY_ID,
+    aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
+    region_name=AWS_S3_REGION_NAME,
     config=Config(signature_version='s3v4')
 )
-print("Bucket location:", s3.get_bucket_location(Bucket=settings.AWS_STORAGE_BUCKET_NAME))
+print("Bucket location:", s3.get_bucket_location(Bucket=AWS_STORAGE_BUCKET_NAME))
 # Включение детального лога boto3# дополнительные папки со статикой
 
 
