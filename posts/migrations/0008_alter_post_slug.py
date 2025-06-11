6fb3cb2fa105b7c100f444e06ceca5713aea1207
Sergei Lisitsyn <autoanastasia@narod.ru>
2025-06-11 17:48:01 +0200

    post migrations is in order

--- posts/migrations/0008_alter_post_slug.py
+++ posts/migrations/0008_alter_post_slug.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('posts', '0007_post_is_archived_post_updated_at'),
+        ('posts', '0001_initial'),
     ]
 
     operations = [
--- posts/migrations/0009_merge_20250523_1947.py
+++ posts/migrations/0009_merge_20250523_1947.py
@@ -6,7 +6,6 @@ from django.db import migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('posts', '0008_advertisement_post_advertisement'),
         ('posts', '0008_alter_post_slug'),
     ]
 
