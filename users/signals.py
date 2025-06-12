dfce69ff81ccdf886681125985bbe91f35c4d5ee
Sergei Lisitsyn <77849154+SergeiLisitsyn@users.noreply.github.com>
2025-06-12 10:12:27 +0200

    Update signals.py
    import Custom User

--- users/signals.py
+++ users/signals.py
@@ -1,7 +1,7 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.conf import settings
-from .models import UserProfile
+from .models import UserProfile, CustomUser
 
 User = settings.AUTH_USER_MODEL  # Используем кастомную модель пользователя
 
